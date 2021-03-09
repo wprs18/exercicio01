508dba0c944f9ac074247b5a80ed3c5606e5b40d
William de Paula <wprs18@hotmail.com>
2021-03-09 17:53:46 -0300

    primeiro commit

---
+++ src/main/java/exercicio01/main/Main.java
@@ -0,0 +1,37 @@
+package exercicio01.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Main app = new Main();
+        app.start();
+
+    }
+
+    private void start() {
+
+        int idade = 15;
+
+        System.out.println("A idade é de " + idade + " anos");
+
+        idade = (idade + 5) * 2;
+
+        System.out.println("Resultado 1: " + idade);
+
+        idade -= 5;
+
+        System.out.println("Resultado 2: " + idade);
+
+        if(idade > 20) {
+
+            System.out.println("A idade é maior que 20");
+        } else {
+
+            System.out.println("A idade é menor que 20");
+        }
+
+    }
+
+
+}
